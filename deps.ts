a92c1d7d968352cf65141fab4fb817b690a377d7
Alex Alikiotis <alexalikiotis5@gmail.com>
2020-11-30 21:35:27 +0200

    updating package registry

--- deps.ts
+++ deps.ts
@@ -2,5 +2,5 @@
 export { default as EventEmitter } from "https://deno.land/x/events/mod.ts";
 
 // third party modules
-export { nanoid } from "https://cdn.skypack.dev/nanoid";
-export { CronosExpression, validate } from "https://cdn.skypack.dev/cronosjs";
+export { nanoid } from "https://jspm.dev/nanoid";
+export { CronosExpression, validate } from "https://jspm.dev/cronosjs";
